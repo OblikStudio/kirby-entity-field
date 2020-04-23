d494c0a3ae9fdc35ebb5b9c0eb02433c9665d3f5
Hristiyan Dodov <h.dodov@gmail.com>
2020-04-23 09:44:43 +0300

    ref: dev setup

--- index.php
+++ index.php
@@ -1,49 +1,8 @@
 <?php
 
-use Kirby\Cms\App;
-use Kirby\Cms\Content;
-use Kirby\Cms\Form;
-use Kirby\Data\Yaml;
-
-App::plugin('oblik/entity-field', [
-    'fields' => [
-        'entity' => [
-            'props' => [
-                'fields' => function (array $fields) {
-                    return $fields;
-                }
-            ],
-            'computed' => [
-                'value' => function () {
-                    $model = Yaml::decode($this->value);
-                    return $this->form($model)->values();
-                },
-                'fields' => function () {
-                    return $this->form()->fields()->toArray();
-                }
-            ],
-            'methods' => [
-                'form' => function (array $values = []) {
-                    return new Form([
-                        'fields' => $this->attrs['fields'],
-                        'values' => $values,
-                        'model'  => $this->model
-                    ]);
-                },
-            ],
-            'save' => function ($value) {
-                return $this->form($value)->data(true);
-            },
-            'validations' => [
-                // needs custom validator to validate the form with:
-                // $this->field()->form($this->requestBody())->errors()
-                // before saving @see https://github.com/getkirby/ideas/issues/277
-            ]
-        ]
-    ],
-    'fieldMethods' => [
-        'toEntity' => function ($field) {
-            return new Content($field->yaml(), $field->parent());
-        }
-    ]
-]);
+if (class_exists('Kirby\Cms\App')) {
+	require 'lib/index.php';
+} else {
+	require_once 'kirby/bootstrap.php';
+	echo kirby()->render();
+}
