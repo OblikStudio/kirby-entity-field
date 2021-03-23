11d85dc0f00434af6f32e74d5367ac0a59d059af
Hristiyan Dodov <h.dodov@gmail.com>
2021-03-23 15:58:43 +0200

    ref: index.php logic

--- index.php
+++ index.php
@@ -1,10 +1,65 @@
 <?php
 
 use Kirby\Cms\App;
+use Kirby\Cms\Content;
+use Kirby\Cms\Form;
+use Kirby\Data\Yaml;
 
-if (class_exists('Kirby\Cms\App')) {
-	App::plugin('oblik/entity-field', require 'lib/index.php');
-} else {
-	require_once 'kirby/bootstrap.php';
-	echo kirby()->render();
-}
+App::plugin('oblik/entity-field', [
+	'options' => [
+		'toggle' => false,
+	],
+	'fields' => [
+		'entity' => [
+			'props' => [
+				'fields' => function (array $fields) {
+					return $fields;
+				},
+				'icon' => function ($icon = false) {
+					return $icon;
+				},
+				'toggle' => function ($toggle = null) {
+					return $toggle ?? option('oblik.entity-field.toggle');
+				},
+			],
+			'computed' => [
+				'value' => function () {
+					$model = Yaml::decode($this->value);
+					return $this->form($model)->values();
+				},
+				'fields' => function () {
+					return $this->form()->fields()->toArray();
+				}
+			],
+			'methods' => [
+				'form' => function (array $values = []) {
+					return new Form([
+						'fields' => $this->attrs['fields'],
+						'values' => $values,
+						'model'  => $this->model
+					]);
+				},
+			],
+			'save' => function ($value) {
+				$data = $this->form($value)->data(true);
+				$dataFiltered = array_filter($data);
+
+				if (count($dataFiltered) > 0) {
+					return $dataFiltered;
+				} else {
+					return null;
+				}
+			},
+			'validations' => [
+				// needs custom validator to validate the form with:
+				// $this->field()->form($this->requestBody())->errors()
+				// before saving @see https://github.com/getkirby/ideas/issues/277
+			]
+		]
+	],
+	'fieldMethods' => [
+		'toEntity' => function ($field) {
+			return new Content($field->yaml(), $field->parent());
+		}
+	]
+]);
