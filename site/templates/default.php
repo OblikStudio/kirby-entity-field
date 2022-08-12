ad41ea1b0f7e9b80f3987e6de19b8826c9d5aa16
Hristiyan Dodov <h.dodov@gmail.com>
2022-08-12 08:31:44 +0300

    Update example content

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,30 @@
-<?php $data = $site->entity()->toEntity() ?>
-<?php echo $data->files()->toFile()->url() ?>
+<?php $siteEntity = $site->entity()->toEntity() ?>
 
-<?php $data = $page->entity()->toEntity() ?>
-<?php echo $data->files()->toFile()->url() ?>
+<h1><?= $siteEntity->text() ?></h1>
+
+<img src="<?= $siteEntity->logo()->toFile()->url() ?>">
+
+<ul>
+	<?php foreach ($siteEntity->structure()->toStructure() as $entry) : ?>
+		<li>
+			<a href="<?= $entry->url() ?>" target="_blank">
+				<?= $entry->url() ?>
+			</a>
+		</li>
+	<?php endforeach ?>
+</ul>
+
+<ul>
+	<?php foreach ($site->structure()->toStructure() as $entry) : ?>
+		<?php
+		$entity = $entry->entity()->toEntity();
+		$nestedEntity = $entity->entity()->toEntity();
+		?>
+		<li>
+			<ul>
+				<li>text: <?= $entity->text() ?></li>
+				<li>nested text: <?= $nestedEntity->text() ?></li>
+			</ul>
+		</li>
+	<?php endforeach ?>
+</ul>
